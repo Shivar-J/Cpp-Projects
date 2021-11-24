7c6645b56348222e8f2fb33345de199b9281df33
Shivar Jadoonanan <77025369+Shivar-J@users.noreply.github.com>
2021-11-24 13:40:32 -0500

    Update main.cpp

--- OddEvenFibonacci/main.cpp
+++ OddEvenFibonacci/main.cpp
@@ -7,8 +7,37 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Enter Number:" << endl;
+    int userNum;
+    cin >> userNum;
+    
+    int firstNum = 0;
+    int secondNum = 1;
+    int tempNum = 0;
+    int evenNum = 0;
+    
+    cout << "Fibonacci Sequence: ";
+    for(int i = 1; i <= userNum+1; i++){
+        if(i == 1){
+            continue;
+        }
+        if(i == 2){
+            cout << secondNum << " ";
+            continue;
+        }
+        if(tempNum % 2 == 0){
+            evenNum += tempNum;
+        }
+        tempNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = tempNum;
+        cout << tempNum << " ";
+    }
+    cout << "\nFinal sum of even numbers: " << evenNum << endl;
+    
     return 0;
 }
