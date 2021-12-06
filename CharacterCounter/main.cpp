0d2ada3b4ce516aeb8bae59617c3cfdcd02cb64e
Shivar Jadoonanan <77025369+Shivar-J@users.noreply.github.com>
2021-12-06 12:08:42 -0500

    Update main.cpp

--- CharacterCounter/main.cpp
+++ CharacterCounter/main.cpp
@@ -1,14 +1,24 @@
-//
-//  main.cpp
-//  CharacterCounter
-//
-//  Created by Shivar Jadoonanan on 2021-12-04.
-//
-
 #include <iostream>
+#include <unordered_map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Enter string" << endl;
+    string input;
+    cin >> input;
+
+    unordered_map<char, int> letterCount;
+    
+    for(int i = 0; i < input.size(); i++){
+        if(letterCount.find(input[i]) == letterCount.end()){
+            letterCount.insert(make_pair(input[i], 1));
+        } else {
+            letterCount[input[i]]++;
+        }
+    }
+    for(auto& i : letterCount){
+        cout << i.first << " " << i.second << "\n";
+    }
 }
