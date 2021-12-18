6b05413635f6e78174cb1342ffb6260d6ef68ba9
Shivar Jadoonanan <sbshivar@gmail.com>
2021-12-18 13:11:03 -0500

    Initial Commit

---
+++ PerfectNumbers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PerfectNumbers
+//
+//  Created by Shivar Jadoonanan on 2021-12-18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
