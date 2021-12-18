240023a712b97d5c262173a10a645a41b576b48c
Shivar Jadoonanan <77025369+Shivar-J@users.noreply.github.com>
2021-12-18 14:17:23 -0500

    Update main.cpp

--- PerfectNumbers/main.cpp
+++ PerfectNumbers/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cout << "Enter Number" << endl;
+    int number;
+    cin >> number;
+    
+    vector<int> tempNums;
+    
+    for(int i = 0; i < number; i++){
+        if(number % i == 0){
+            cout << i << " ";
+            tempNums.push_back(i);
+        }
+    }
+    cout << endl;
+    
+    int temp = 0;
+    
+    for(int i = 0; i < tempNums.size(); i++){
+        temp = tempNums[i] + temp;
+    }
+    
+    if(temp == number){
+        cout << "Perfect number" << endl;
+    } else {
+        cout << "Not a perfect number" << endl;
+    }
 }
