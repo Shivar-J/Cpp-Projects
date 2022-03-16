92f367f4da61693dfa8df83051d6db7421f4b749
Shivar Jadoonanan <sbshivar@gmail.com>
2022-03-16 14:07:38 -0400

    added TransposingMatrix

--- TransposingMatrix/main.cpp
+++ TransposingMatrix/main.cpp
@@ -6,9 +6,49 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void Print(vector<vector<int>> matrix) {
+    for(int i = 0; i < matrix.size(); i++) {
+        for(int j = 0; j < matrix.size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Transpose(vector<vector<int>> matrix, int size) {
+    vector<vector<int>> tempVec (size, vector<int> (size));
+    
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            tempVec[i][j] = matrix[j][i];
+            cout << tempVec[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int size;
+    cin >> size;
+    
+    vector<vector<int>> matrix (size, vector<int> (size));
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++) {
+            int num;
+            cin >> num;
+            matrix[i][j] = num;
+        }
+    }
+    cout << "Original Matrix: " << endl;
+    Print(matrix);
+    
+    cout << "Transposed Matrix: " << endl;
+    Transpose(matrix, size);
 }
