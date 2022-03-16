893fc58cce9570e5e415e362554c1665c34caa97
Shivar Jadoonanan <sbshivar@gmail.com>
2022-03-16 14:20:23 -0400

    added Challenge Problem 'Sentences'

--- Sentences/main.cpp
+++ Sentences/main.cpp
@@ -6,9 +6,26 @@
 //
 
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    string sentence;
+    getline(cin, sentence);
+    int end = sentence.find("and");
+    int start = sentence.find(" ");
+    replace(sentence.begin(), sentence.end(), ',', '\0');
+    replace(sentence.begin()+start, sentence.begin()+(start + 1), ' ', '*');
+    start = sentence.find(" ");
+    replace(sentence.begin()+start, sentence.begin()+(start + 1), ' ', '*');
+    replace(sentence.begin(), (sentence.begin() + end), ' ', ',');
+    //replace(sentence.begin(), sentence.end(), 0, ',');
+    replace(sentence.begin(), sentence.begin() + (start + 1), '*', ' ');
+    cout << sentence << endl;
 }
+
